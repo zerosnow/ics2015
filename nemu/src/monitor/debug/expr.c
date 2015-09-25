45807dee1eefac80e384a181f93f6ab27a7be9fc
tracer-ics2015 <tracer@njuics.org>
2015-09-25 22:43:16 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-63-generic #103-Ubuntu SMP Fri Aug 14 21:43:30 UTC 2015 i686 athlon i686 GNU/Linux  22:43:16 up 1 day,  1:17,  3 users,  load average: 0.26, 0.17, 0.15 6f6fe6ab788e9b48e4c3b0d0a8067eedde9c137

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -115,6 +115,7 @@ static bool make_token(char *e) {
 						nr_token++;
 						break;
 				case HEX:
+						printf("%s\n",tokens[nr_token].str);
 						middleTran=strtol(tokens[nr_token].str+2, NULL, 16);
 						sprintf(tokens[nr_token].str, "%d", middleTran);
 						printf("%d\n",middleTran);
