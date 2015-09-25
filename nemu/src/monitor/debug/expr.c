2751f9fe2987893c099bf98605eca2bbfd95950b
tracer-ics2015 <tracer@njuics.org>
2015-09-25 11:51:25 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-63-generic #103-Ubuntu SMP Fri Aug 14 21:43:30 UTC 2015 i686 athlon i686 GNU/Linux  11:51:25 up 14:25,  3 users,  load average: 0.29, 0.65, 0.62 30859125805a2f849def8353b60c330e11ab31bf

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -30,7 +30,7 @@ static struct rule {
 	{"\\*", '*'},
 	{"\\/", '/'},
 	{"==", EQ},						// equal
-	{"[0-9]*$",NUM},					//number
+	{"^[0-9]*$",NUM},					//number
 	{"\\$[a-z]",REG},					//register
 	{"(", '('},	
 	{")", ')'}
