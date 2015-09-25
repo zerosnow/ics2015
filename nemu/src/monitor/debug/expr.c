8635c5fabf6387e7e18cd38176094ae5ab58872e
tracer-ics2015 <tracer@njuics.org>
2015-09-25 11:41:59 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-63-generic #103-Ubuntu SMP Fri Aug 14 21:43:30 UTC 2015 i686 athlon i686 GNU/Linux  11:41:59 up 14:16,  3 users,  load average: 1.16, 0.78, 0.53 5ae6394ef8ae677236ec8516108446f5fa413282

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -114,7 +114,7 @@ static bool make_token(char *e) {
 						tokens[nr_token].type=rules[i].token_type;
 						strncpy(tokens[nr_token].str, substr_start, substr_len);
 						nr_token++;
-						panic("please implement me");
+						//panic("please implement me");
 				}
 
 				break;
