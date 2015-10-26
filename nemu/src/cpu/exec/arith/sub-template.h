95b6c629a99f18f949e094f3a806808bb28564b1
tracer-ics2015 <tracer@njuics.org>
2015-10-26 20:49:35 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  20:49:35 up 29 min,  2 users,  load average: 0.34, 0.41, 0.38 10317f7152ded2cc193fb5d626e7124dfd569998

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -3,12 +3,21 @@
 #define instr sub
 
 static void do_execute() {
-	DATA_TYPE result = op_dest->val - (DATA_TYPE)op_src->val;
+	DATA_TYPE result = op_dest->val - op_src->val;
 	OPERAND_W(op_dest, result);
 	
 	print_asm_template1();
 }
 
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+
 make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+make_instr_helper(i2a)
+
 
 #include "cpu/exec/template-end.h"
