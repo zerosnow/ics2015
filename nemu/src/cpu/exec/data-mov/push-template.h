0d6acfe50840ff19e6ba1c10ec3835ce7b403778
tracer-ics2015 <tracer@njuics.org>
2015-10-27 08:30:10 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  08:30:10 up 17 min,  3 users,  load average: 0.69, 0.39, 0.34 279fc8bbccb12afca0e8ed3674387159d904068

---
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -0,0 +1,22 @@
+#include "cpu/exec/template-start.h"
+
+#define instr push
+
+static void do_execute () {
+	#if DATA_BYTE == 2
+		cpu.esp -=2;
+		MEM_W(cpu.esp, op_src->val);
+	#elif DATA_BYTE == 4
+		cpu.esp -= 4;
+		MEM_W(cpu.esp, op_src->val);
+	#endif
+
+	/* TODO: Update EFLAGS. */
+	//panic("please implement me");
+
+	print_asm_template1();
+}
+
+make_instr_helper(r)
+
+#include "cpu/exec/template-end.h"
