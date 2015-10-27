abaf535f5966a076b65ae9866dbfd77277b7d24c
tracer-ics2015 <tracer@njuics.org>
2015-10-27 10:04:35 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  10:04:35 up  1:51,  3 users,  load average: 0.35, 0.41, 0.51 e6bce6f7c868eb7e065e64ecf946af5cfdccfd5

--- nemu/src/cpu/exec/arith/test-template.h
+++ nemu/src/cpu/exec/arith/test-template.h
@@ -3,8 +3,13 @@
 #define instr test
 
 static void do_execute () {
-	
-
+	DATA_TYPE result = op_src->val & op_dest->val;
+	cpu.CF = 0;
+	cpu.PF = ~( (result & 0x1) & ((result>>1) & 0x1) & ((result>>2) & 0x1) & ((result>>3) & 0x1) & 
+	((result>>4) & 0x1) & ((result>>5) & 0x1) & ((result>>6) & 0x1) & ((result>>7) & 0x1));
+	cpu.ZF =  (result == 0);
+	cpu.SF = MSB(result);
+	cpu.OF = 0;
 	/* TODO: Update EFLAGS. */
 	//panic("please implement me");
 
