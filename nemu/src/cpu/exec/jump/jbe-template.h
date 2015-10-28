d88e40c51270a1df59002ec40bc8c626c7b6fcd2
tracer-ics2015 <tracer@njuics.org>
2015-10-28 19:34:41 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  19:34:41 up 56 min,  3 users,  load average: 0.76, 0.43, 0.34 55e7d2b8bd38cabb34cc8fddf9ca191f9a687dd8

--- nemu/src/cpu/exec/jump/jbe-template.h
+++ nemu/src/cpu/exec/jump/jbe-template.h
@@ -5,10 +5,9 @@
 static void do_execute() {
 	if (cpu.CF == 1 || cpu.ZF == 1)
 	{
+		printf("%x\n", op_src->addr);
 		cpu.eip += op_src->addr;
-		#if DATA_BYTE == 1
-			cpu.eip &= 0x000000ff;
-		#elif DATA_BYTE == 2
+		#if DATA_BYTE == 2
 			cpu.eip &= 0x0000ffff;
 		#endif
 	}
