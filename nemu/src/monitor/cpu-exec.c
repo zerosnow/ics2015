5fec405772f68113f00c60e21db55dcf53ab2ba4
tracer-ics2015 <tracer@njuics.org>
2015-10-29 22:54:27 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  22:54:27 up  4:58,  2 users,  load average: 0.16, 0.29, 0.28 dfdf2f6f7902b5521045332751424e87ff6ca5b1

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -59,10 +59,8 @@ void cpu_exec(volatile uint32_t n) {
 
 		/* Execute one instruction, including instruction fetch,
 		 * instruction decode, and the actual execution. */
-		printf("%x\n", cpu.eip);
 		int instr_len = exec(cpu.eip);
 		cpu.eip += instr_len;
-		printf("%x\n", cpu.eip);
 
 #ifdef DEBUG
 		print_bin_instr(eip_temp, instr_len);
