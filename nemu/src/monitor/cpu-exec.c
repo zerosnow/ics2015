28f00cca2d293c8d820d56d2c31b29efe6a3fce2
tracer-ics2015 <tracer@njuics.org>
2015-10-29 17:09:35 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  17:09:35 up  1:29,  2 users,  load average: 2.61, 2.55, 2.92 b838dfc64aaf3aaec5f95192cf34b99d7dee6a2a

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -8,7 +8,7 @@
  * This is useful when you use the ``si'' command.
  * You can modify this value as you want.
  */
-#define MAX_INSTR_TO_PRINT 20
+#define MAX_INSTR_TO_PRINT 50
 
 int nemu_state = STOP;
 int exec(swaddr_t);
