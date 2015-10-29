3e138d2eef2cf7651f9c49fe44a0c0fe42aba523
tracer-ics2015 <tracer@njuics.org>
2015-10-29 22:27:20 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  22:27:20 up  4:31,  2 users,  load average: 0.24, 0.19, 0.28 efcb4479ded2f74b811db1624d6eccfe33ae424

--- nemu/src/cpu/exec/logic/xor-template.h
+++ nemu/src/cpu/exec/logic/xor-template.h
@@ -6,8 +6,12 @@ static void do_execute () {
 	DATA_TYPE result = op_dest->val ^ op_src->val;
 	OPERAND_W(op_dest, result);
 
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	cpu.CF = 0;
+	cpu.OF = 0;
+	cpu.PF = ~( (result & 0x1) & ((result>>1) & 0x1) & ((result>>2) & 0x1) & ((result>>3) & 0x1) & 
+	((result>>4) & 0x1) & ((result>>5) & 0x1) & ((result>>6) & 0x1) & ((result>>7) & 0x1));
+	cpu.SF = MSB(result);
+	cpu.ZF = (result == 0); 
 
 	print_asm_template2();
 }
