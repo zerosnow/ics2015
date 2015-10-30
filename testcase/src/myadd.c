f55008d0e525752185daf39d39f745d1135816a6
tracer-ics2015 <tracer@njuics.org>
2015-10-30 15:02:21 +0800

    >  run 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  15:02:21 up  1:04,  2 users,  load average: 0.28, 0.33, 0.23 840a22b655b9ef16cedd7a66750525635e434aa

--- testcase/src/myadd.c
+++ testcase/src/myadd.c
@@ -1,15 +1,16 @@
 #include "trap.h"
 
-int add(int x, int y) {
-	return x+y;
+int add(int x, int y, int z) {
+	return x+y+z;
 }
 
 int caller() {
 	int temp1 = 125;
 	int temp2 = 80;
+	int temp3 = 40;
 	int sum;
 	set_bp();
-	sum = add(temp1, temp2);
+	sum = add(temp1, temp2, temp3);
 	return sum;
 }
 
