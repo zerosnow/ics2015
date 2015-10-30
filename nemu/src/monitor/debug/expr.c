5facfa03a0ac334bdfbe65fa457b9d51a04e4cb4
tracer-ics2015 <tracer@njuics.org>
2015-10-30 12:49:40 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  12:49:40 up 38 min,  3 users,  load average: 0.26, 0.27, 0.24 b2cbf9f429c83c8b14789cf05fe87a9770cbe0b6

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -190,6 +190,7 @@ static bool make_token(char *e) {
 						for(i=0;i<nr_symtab_entry;i++) {
 							if (symtab[i].st_info == 17 && strcmp((char *)&strtab[symtab[i].st_name], tokens[nr_token].str) == 0)
 							{
+								printf("%d\n", symtab[i].st_value);
 								sprintf(tokens[nr_token].str, "%d", symtab[i].st_value);
 								tokens[nr_token].type = NUM;
 								break;
