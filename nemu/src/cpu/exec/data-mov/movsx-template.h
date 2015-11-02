ae71c0fd183b128d469e32af3325bd313b56a3a7
tracer-ics2015 <tracer@njuics.org>
2015-11-02 18:24:49 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  18:24:49 up 4 min,  2 users,  load average: 0.46, 0.54, 0.25 917533a6ffbfd53375b59fea598d869188028018

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -3,14 +3,14 @@
 #define instr movsx
 
 static void do_execute () {
-	if (MSB(op_src->val))
-	{
-		#if DATA_BYTE == 1
-			op_src->val = 0xffffff00 | op_src->val;
-		#elif DATA_BYTE == 2
-			op_src->val = 0xffff0000 | op_src->val;
-		#endif
-	}
+	// if (MSB(op_src->val))
+	// {
+	// 	#if DATA_BYTE == 1
+	// 		op_src->val = 0xffffff00 | op_src->val;
+	// 	#elif DATA_BYTE == 2
+	// 		op_src->val = 0xffff0000 | op_src->val;
+	// 	#endif
+	// }
 	write_operand_l(op_dest, op_src->val);
 	print_asm_template2();
 }
