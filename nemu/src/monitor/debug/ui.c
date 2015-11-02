f84ed3eb0a46e9240e5124f8333f00a96df20b4a
tracer-ics2015 <tracer@njuics.org>
2015-11-02 14:11:17 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  14:11:17 up 14 min,  3 users,  load average: 0.05, 0.20, 0.22 6995f36fcc053032d772f3fcb645369c28fb241

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -210,6 +210,7 @@ static int cmd_bt(char *args) {
 		tempStactFrame.prev_ebp = swaddr_read(temp_ebp, 4);
 		tempStactFrame.cur_addr = tempStactFrame.ret_addr?tempStactFrame.ret_addr:cpu.eip;
 		for(i=0;i<nr_symtab_entry;i++) {
+			printf("%d\n", symtab[i].st_info);
 			if (symtab[i].st_info == 18 && tempStactFrame.cur_addr >= symtab[i].st_value && tempStactFrame.cur_addr <=symtab[i].st_value + symtab[i].st_size) {
 				strcpy(tempStactFrame.cur_funcName ,(char *)&strtab[symtab[i].st_name]);
 				tempStactFrame.begin_addr = symtab[i].st_value;
