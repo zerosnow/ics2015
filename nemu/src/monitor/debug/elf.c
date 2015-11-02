7335a0146e8674726307051d73524f37662a0c18
tracer-ics2015 <tracer@njuics.org>
2015-11-02 14:15:12 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  14:15:12 up 18 min,  2 users,  load average: 0.21, 0.22, 0.23 74aeed3365f1fadd7a92de4df3d85b488092fc6

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -12,7 +12,7 @@ void load_elf_tables(int argc, char *argv[]) {
 	int ret;
 	Assert(argc == 2, "run NEMU with format 'nemu [program]'");
 	exec_file = argv[1];
-	printf("%s\n", exec_file);
+	printf("11111%s\n", exec_file);
 	FILE *fp = fopen(exec_file, "rb");
 	Assert(fp, "Can not open '%s'", exec_file);
 
