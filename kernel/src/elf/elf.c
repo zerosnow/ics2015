30d9384f1abc248b4aab04a886f86a1835573820
tracer-ics2015 <tracer@njuics.org>
2015-11-03 08:17:35 +0800

    >  compile kernel 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  08:17:35 up 15 min,  2 users,  load average: 0.56, 0.31, 0.25 111b1b005f6b2760594f10bde6e251218cbf6665

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -37,8 +37,8 @@ uint32_t loader() {
 
 	/* Load each program segment */
 	//panic("please implement me");
+	ph = (void *)(buf + elf->e_phoff);
 	for(i=0; i<elf->e_phnum;i++ ) {
-		ph = (void *)(buf + elf->e_phoff);
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
