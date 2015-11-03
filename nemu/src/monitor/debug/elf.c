63df1cd0dfb966bc1818c223a7e627bea2518f2a
tracer-ics2015 <tracer@njuics.org>
2015-11-03 08:23:22 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  08:23:22 up 21 min,  2 users,  load average: 0.21, 0.25, 0.24 7f5e0927e8474b16ce4917f7d6724d69b4b6407c

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -19,7 +19,9 @@ void load_elf_tables(int argc, char *argv[]) {
 	/* Read the first 4096 bytes from the exec_file.
 	 * They should contain the ELF header and program headers. */
 	ret = fread(buf, 4096, 1, fp);
-	assert(ret == 1);
+	if (!feof(fp)) {
+		assert(ret == 1);
+	}
 
 	/* The first several bytes contain the ELF header. */
 	Elf32_Ehdr *elf = (void *)buf;
