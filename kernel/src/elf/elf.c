8bc7878ee3e5fefe6c0688fb9dcb665389fffe3b
tracer-ics2015 <tracer@njuics.org>
2015-11-08 17:07:35 +0800

    >  compile kernel 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  17:07:35 up 20 min,  2 users,  load average: 0.41, 0.40, 0.39 3cd039f0cdbc7f6741a40abe1c8c988c5ba80ee

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -33,7 +33,7 @@ uint32_t loader() {
 
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0x7f454c46;
+	const uint32_t elf_magic = 0x4c467f45;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
