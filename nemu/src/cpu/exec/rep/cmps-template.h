d122aa5ca1eb01f7feda5795347dc82ba3b44aaf
tracer-ics2015 <tracer@njuics.org>
2015-11-08 22:02:12 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  22:02:12 up  5:15,  2 users,  load average: 0.19, 0.17, 0.22 3f309dd3c6136a0b68c0aac21be1d703eadf22b4

--- nemu/src/cpu/exec/rep/cmps-template.h
+++ nemu/src/cpu/exec/rep/cmps-template.h
@@ -5,7 +5,7 @@
 make_helper(concat3(instr, _, SUFFIX)) {
 	if (swaddr_read(cpu.edi,  DATA_BYTE) == swaddr_read(cpu.esi, DATA_BYTE)) {
 		cpu.edi += DATA_BYTE;
-		cpu.esi +=DATA_BYTE;
+		cpu.esi += DATA_BYTE;
 		return 1;
 	}
 	else return 0;
