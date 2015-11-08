281830b6138782d788b47e4cdc160e7f36049a75
tracer-ics2015 <tracer@njuics.org>
2015-11-08 14:36:45 +0800

    >  run 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  14:36:45 up  4:38,  3 users,  load average: 0.21, 0.19, 0.22 6b6b839a47c46a5246651649199fea67373fee6

--- testcase/src/string.c
+++ testcase/src/string.c
@@ -19,27 +19,8 @@ int main() {
 	//nemu_assert(strcmp(s[0] + 1, s[1] + 1) == -1);
 	//nemu_assert(strcmp(s[0] + 2, s[1] + 2) == -1);
 	//nemu_assert(strcmp(s[0] + 3, s[1] + 3) == -1);
-	char *ss = strcat(strcpy(str, str1), s[3]);
-	nemu_assert(ss[0] == 'H');
-	nemu_assert(ss[1] == 'e');
-	nemu_assert(ss[2] == 'l');
-	nemu_assert(ss[3] == 'l');
-	nemu_assert(ss[4] == 'o');
-	nemu_assert(ss[5] == ',');
-	nemu_assert(ss[6] == ' ');
-	nemu_assert(ss[7] == 'W');
-	nemu_assert(ss[8] == 'o');
-	nemu_assert(ss[9] == 'r');
-	nemu_assert(ss[10] == 'l');
-	nemu_assert(ss[11] == 'd');
-	nemu_assert(ss[12] == '!');
-	nemu_assert(ss[13] == '\n');
-	nemu_assert(ss[14] == '\0');
-	nemu_assert(strcmp(ss, s[4]) == 0);
 
-	//nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
-	//nemu_assert(strcmp(strcpy(str, str1), str1) == 0);
-	//nemu_assert(strcmp(strcat(str1, s[3]), s[4]) == 0);
+	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
 	//nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
 
 	HIT_GOOD_TRAP;
