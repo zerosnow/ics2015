b782006ba6d5af04fc4963fc7593bd2bd7fa7527
tracer-ics2015 <tracer@njuics.org>
2015-11-09 20:27:28 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  20:27:28 up 16 min,  2 users,  load average: 0.30, 0.40, 0.38 183770247b3939775823187f8ad447b8c397b540

--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -4,6 +4,7 @@
 
 static void do_execute () {
 	DATA_TYPE result = op_dest->val & op_src->val;
+	printf("%d, %d, %d, %d\n", op_src->val, op_dest->val, op_src->type, OP_TYPE_SIMM);
 	OPERAND_W(op_dest, result);
 
 	cpu.CF = 0;
