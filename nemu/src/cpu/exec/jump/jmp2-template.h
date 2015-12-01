f3995fb026860d0c726fed075693884fbe7dde00
tracer-ics2015 <tracer@njuics.org>
2015-12-01 11:03:01 +0800

    >  compile NEMU 141220000 purple Linux purple-ThinkPad-Edge-E435 3.13.0-66-generic #108-Ubuntu SMP Wed Oct 7 15:21:40 UTC 2015 i686 athlon i686 GNU/Linux  11:03:01 up 43 min,  3 users,  load average: 0.45, 0.72, 0.70 7452112e98a614aa48e45099b0022ebe57b241f1

--- nemu/src/cpu/exec/jump/jmp2-template.h
+++ nemu/src/cpu/exec/jump/jmp2-template.h
@@ -6,8 +6,10 @@ static void do_execute() {
 	#if DATABYTE == 2 
 		cpu.eip = op_src->val & 0x0000ffff - 2;
 	#else
-		cpu.eip = op_src->val - 2;
-		printf("%x, %d, %d\n", cpu.eip, OP_TYPE_MEM, op_src->type);
+		if (op_src->type == OP_TYPE_MEM) 
+			cpu.eip = op_src->val;
+		else
+			cpu.eip = op_src->val - 2;
 	#endif
 	print_asm_template1();
 }
