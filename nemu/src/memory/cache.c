2fcea0eafcdb9f1cf016fb95fe9d0faa91233a32
tracer-ics2015 <tracer@njuics.org>
2015-12-28 20:53:06 +0800

    >  compile NEMU 141220000 pruple Linux purple 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  20:53:06 up  2:50,  2 users,  load average: 0.17, 0.34, 0.39 99d8186e165378fc37cd3de2ca5f3851228c2ce2

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -89,7 +89,7 @@ void pretend_cache_read(hwaddr_t addr, size_t len) {
 		if (cache[caddr.r][i].q == caddr.q && cache[caddr.r][i].f == caddr.f && cache[caddr.r][i].valid == 1) {
 			if (len + caddr.w <= 64) {
 				memcpy(&temp, &cache[caddr.r][i].block[caddr.w], len);
-				printf("content = %d, f = %d, q = %d", temp, caddr.f , caddr.q);
+				printf("content = %x, f = %d, q = %d\n", temp, caddr.f , caddr.q);
 				return ;
 			}
 			
