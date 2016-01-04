551bca0f3cd0bb5ff27c9cae5b8621b9ae558d0e
tracer-ics2015 <tracer@njuics.org>
2016-01-04 19:31:04 +0800

    >  compile kernel 141220000 pruple Linux purple 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  19:31:04 up  1:20,  2 users,  load average: 0.52, 0.50, 0.50 366efc252914ceceb30c7b318f04557cd1fb854

--- nemu/src/cpu/exec/misc/misc.c
+++ nemu/src/cpu/exec/misc/misc.c
@@ -27,6 +27,13 @@ make_helper(lea) {
 
 make_helper(cld) {
 	cpu.DF = 0;
+	print_asm("cld");
+	return 1;
+}
+
+make_helper(std) {
+	cpu.DF = 1;
+	print_asm("std");
 	return 1;
 }
 
--- nemu/src/cpu/exec/misc/misc.h
+++ nemu/src/cpu/exec/misc/misc.h
@@ -5,6 +5,7 @@ make_helper(nop);
 make_helper(int3);
 make_helper(lea);
 make_helper(cld);
+make_helper(std);
 make_helper(cwd_cdq);
 make_helper(lgdt);
 
