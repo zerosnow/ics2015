fbbd0251bc922d6abb5aca564aabe85a206e9671
tracer-ics2015 <tracer@njuics.org>
2016-01-05 20:41:48 +0800

    >  compile kernel 141220000 pruple Linux purple 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  20:41:48 up  2:24,  2 users,  load average: 0.37, 0.44, 0.39 f8de91eb98a791f276755e9565c07fba7ea151e

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -100,8 +100,7 @@ void init_cond() {
 	/* Keep the `bt' command happy. */
 	asm volatile("movl $0, %ebp");
 	asm volatile("subl $16, %esp");
-	nemu_assert(0);
-	
+
 	/* Here we go! */
 	((void(*)(void))eip)();
 
