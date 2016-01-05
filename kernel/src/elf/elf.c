bcf474f239f0394cfa8147b33e33e2567c19a42c
tracer-ics2015 <tracer@njuics.org>
2016-01-05 18:58:39 +0800

    >  compile kernel 141220000 pruple Linux purple 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  18:58:39 up 41 min,  2 users,  load average: 1.11, 0.48, 0.28 af99c84acba11a518ea48c2184d4f0842c223e3c

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -37,13 +37,13 @@ uint32_t loader() {
 
 	/* Load each program segment */
 	ph = (Elf32_Phdr *)(buf + elf->e_phoff);
-	nemu_assert(elf->e_entry > 0x8048000);
+	nemu_assert(elf->e_entry > 0xc0000000);
 	for(i=0; i<elf->e_phnum;i++) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 			mm_malloc((uint32_t)ph->p_vaddr, ph->p_memsz);
-			 ramdisk_read((uint8_t *)(ph->p_vaddr), ph->p_offset, ph->p_filesz);
-			 memset((void *)(ph->p_vaddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+			ramdisk_read((uint8_t *)(ph->p_vaddr), ph->p_offset, ph->p_filesz);
+			memset((void *)(ph->p_vaddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t brk;
