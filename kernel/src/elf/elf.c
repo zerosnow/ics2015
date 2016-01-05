b35addb521a96bae2222d3d9f96b72f30c36d19f
tracer-ics2015 <tracer@njuics.org>
2016-01-05 11:33:56 +0800

    >  compile kernel 141220000 pruple Linux purple 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  11:33:56 up  1:19,  2 users,  load average: 0.45, 0.31, 0.33 fe215bdedea1ec9c6224444dd2c9df589da421fc

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -40,7 +40,7 @@ uint32_t loader() {
 	for(i=0; i<elf->e_phnum;i++) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
-			nemu_assert(ph->p_vaddr < 0xc0100000);
+			nemu_assert(ph->p_vaddr == 0x8048000);
 			 ramdisk_read((uint8_t *)(ph->p_vaddr), ph->p_offset, ph->p_filesz);
 			 memset((void *)(ph->p_vaddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 #ifdef IA32_PAGE
